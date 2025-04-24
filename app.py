5b6ae1daee1b01d0dfc272e8f5fa98e0a238cd1c
Breno Reis <reisb6774@gmail.com>
2025-04-24 13:26:03 -0700

    feat!: Introducing the python algorithym

---
+++ app.py
@@ -0,0 +1,6 @@
+number = int ( input ('Enter the number that you desire:') )
+
+if number % 11 == 0 :
+    print(f'The integer {number} is divisible by 11')
+else:
+    print(f'The integer {number} is not divisible by 11')
